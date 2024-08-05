db83499532a760f1176448d911356602dfb8d8de
jj_ <avasoldier2@gmail.com>
2024-08-05 19:56:03 +0900

    ✨ renameコマンドの追加

---
+++ cmd/rename.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// renameCmd represents the rename command
+var renameCmd = &cobra.Command{
+	Use:   "rename <OLD NAME> <NEW NAME>",
+	Short: "rename workspace",
+	Long:  `rename workspace`,
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		oldName := args[0]
+		newName := args[1]
+
+		usecase, err := NewUsecase(baseFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+
+		if err := usecase.RenameWorkspace(oldName, newName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(renameCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// renameCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// renameCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
