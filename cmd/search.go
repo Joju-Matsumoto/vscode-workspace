ac364cdb0246c432a5a06a5d3ed7feffac81b22a
jj_ <avasoldier2@gmail.com>
2024-08-05 19:32:59 +0900

    ✨ searchコマンドの修正
    - 複数ディレクトリを指定できるようにした
    - 指定しない場合はカレントディレクトリを使用する
    - ヘルプメッセージの修正

--- cmd/search.go
+++ cmd/search.go
@@ -4,33 +4,39 @@ Copyright © 2024 jj_ <jj_@team-jj.com>
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/Joju-Matsumoto/vscode-workspace/vscodeworkspace"
 	"github.com/spf13/cobra"
 )
 
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
-	Use:   "search [BASE DIRECTORY]",
+	Use:   "search [BASE DIRECTORY]...",
 	Short: "search workspaces",
-	Long:  `search workspaces`,
+	Long: `search workspaces
+
+  With no [BASE DIRECTOR], current directory is used.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			return fmt.Errorf("no base directory specified")
+		dirs := args
+		if len(dirs) == 0 {
+			dirs = append(dirs, ".")
 		}
-		baseDir := args[0]
 
 		usecase, err := NewUsecase(baseFile)
 		if err != nil {
 			return err
 		}
 
-		wss, err := usecase.SearchWorkspaceFromDirectory(baseDir, "")
-		if err != nil {
-			return err
+		workspaces := make([]vscodeworkspace.Workspace, 0)
+
+		for _, dir := range dirs {
+			wss, err := usecase.SearchWorkspaceFromDirectory(dir, "")
+			if err != nil {
+				return err
+			}
+			workspaces = append(workspaces, wss...)
 		}
 
-		ShowWorkspaces(wss...)
+		ShowWorkspaces(workspaces...)
 
 		return nil
 	},
